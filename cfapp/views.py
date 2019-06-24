69f07115b051fd3497c3df518d4033933c005f79
Armando H <mexarm@me.com>
2019-06-24 16:58:38 -0500

    before refactor models to remove tenant

--- cfapp/views.py
+++ cfapp/views.py
@@ -1,13 +1,27 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, mixins
 #from rest_framework.decorators import action
 #from rest_framework.response import Response
 from cfapp import models
 from cfapp import serializers
 
-from cfapp.permissions import IsOwner
+# from cfapp.permissions import IsOwner
+
+# mixins.CreateModelMixin,
+# mixins.RetrieveModelMixin,
+# mixins.UpdateModelMixin,
+# mixins.DestroyModelMixin,
+# mixins.ListModelMixin,
+# GenericViewSet
+
+# class TagViewSet(viewsets.ModelViewSet):
 
 
-class TagViewSet(viewsets.ModelViewSet):
+class TagViewSet(mixins.CreateModelMixin,
+                 mixins.RetrieveModelMixin,
+                 #  mixins.UpdateModelMixin,
+                 #  mixins.DestroyModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     serializer_class = serializers.TagSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
